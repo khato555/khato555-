5936fe46f7b5f2219686dd363c74f80f4ff91ab1
khato555 <162362613+khato555@users.noreply.github.com>
2024-03-08 08:26:41 +0000

    add seskh.js file

---
+++ seskh.js
@@ -0,0 +1,29 @@
+let a=23;
+console.log(a);
+let b=4;
+console.log(b);
+console.log(a+b);
+console.log(a*b);
+let c=a>b;
+console.log(c);
+
+
+
+let person = {
+    "first name":"khatuna",
+"last name":"dzidzaria",
+"age":38,
+"address":{
+    "city":"walenjixa",
+    "country":"jvari"},
+  "phone no":595302273,
+  "email":"x.dzidzaria85@gmail.com",
+  "friends":{
+    "friend1":"nino",
+    "friend2":"ana",
+    "friend3":"lika"
+  }
+} ;
+
+console.log(person.friends);
+console.log(person["first name"]);
