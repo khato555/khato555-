4bb1deb145f58929bed99e21be96560289ee83be
khato555 <162362613+khato555@users.noreply.github.com>
2024-03-11 20:00:50 +0000

    add test.js file

---
+++ test.js
@@ -0,0 +1,64 @@
+/*1*/
+var a=87,
+ b=65;
+console.log (a+b);
+
+/*2,3*/
+let student={
+    firstname:"nino",
+    lastname:"bochorishvili",
+    age:24,
+    email:"n.bochorishvili@gmail.com",
+    marks:"hell"
+
+}
+console.log(student.firstname);
+
+/*4*/
+let m=45,n=23;
+
+if (m>n) {
+    console.log(m+n)
+} else {
+    console.log(n)
+}
+
+/*5*/
+let studentname="khatuna";
+
+if (studentname="khatuna")
+{console.log("Rise and shine",studentname,"!")
+}else{
+console.log("Rise and shine")
+}
+
+/*6*/
+let salary=1000;
+bonus="80%";
+
+if (salary>=2000){
+ console.log(bonus=0)
+}else {
+console.log(bonus="10%")
+}
+
+/*7*/
+var
+ a=23,
+b=a/2,
+c=a+b;
+console.log(c);
+
+/*8,*/
+let phones=["Iphone","Samsung","Pixel","Huawei","Xiaomi"]
+console.log(phones);
+
+/*9-დავამატოთ ახალი ელემენტი */
+phones.push("Motorola");
+console.log(phones);
+
+/*10*/
+for (leti=1;i<13;i++) {
+    console.log(i)
+    };
+
